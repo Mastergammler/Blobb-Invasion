5c3cb91c68bb3070982354f4770ab5e3320d8424
mastergammler <raphael-hoppe@gmx.de>
2020-04-14 17:18:30 +0200

    Added now background for inventory ui

---
+++ Assets/Gameplay/Managers/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager mInstance;
+    public static UIManager Instance 
+    {
+        get
+        {
+            if(mInstance == null) throw new UnityException("Trying to access UI manager b4 game object is initialized");
+            return mInstance;
+        }
+        private set
+        {
+            mInstance = value;
+        }
+    }
+
+    private void Awake() {
+        Instance = this;    
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
