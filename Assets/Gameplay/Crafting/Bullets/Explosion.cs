ec499cb8d64e33505af9452e68075e14eb7ee63e
mastergammler <raphael-hoppe@gmx.de>
2020-04-19 12:30:32 +0200

    Adjusted explosion effects & range

--- Assets/Gameplay/Crafting/Bullets/Explosion.cs
+++ Assets/Gameplay/Crafting/Bullets/Explosion.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Explosion : MonoBehaviour, IExplosion
 {
+
+    public bool AddScreenShake = true;
+    public float ScreenShakeAmplitude = 1.5f;
+    public float FrequencyRamp = 10f;
+    public float ShakeTime = 1f;
+    public CinemachineVirtualCamera Camera;
+
+
     private const float DESTORY_DELAY = 2.5f;
     private const float IMPACT_DELAY = 0.2f;
     private float explosionDamage;
 
     void Start()
     {
+        Camera = GameObject.FindGameObjectWithTag(Tags.MAIN_CAMERA).GetComponentInChildren<CinemachineVirtualCamera>();
         StartCoroutine(destroyAfterDelay());
+        StartCoroutine(screeShakeWithAutoDisable());
     }
+
+    private IEnumerator screeShakeWithAutoDisable()
+    {
+        if(Camera != null)
+        {
+            var noise = Camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            noise.m_AmplitudeGain = ScreenShakeAmplitude;
+            noise.m_FrequencyGain = FrequencyRamp;
+            yield return new WaitForSeconds(ShakeTime);
+            noise.m_AmplitudeGain = 0;
+            noise.m_FrequencyGain = 0;
+        }
+        yield return null;
+    }
+
+
 
     public void SetDamage(float damage)
     {
--- Assets/Utils/Tags.cs
+++ Assets/Utils/Tags.cs
@@ -2,4 +2,5 @@ public static class Tags
 {
     public const string ENEMY = "Enemy";
     public const string PLAYER = "Player";
+    public const string MAIN_CAMERA = "MainCamera";
 }
