d267f0c26279d4c6ef97a978c3b8c87a979cec68
mastergammler <raphael-hoppe@gmx.de>
2020-04-25 15:12:40 +0200

    Added unti tests linked new assemblies and references

---
+++ Assets/08_Test/testscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using BlobbInvasion.Utilities;
+using BlobbInvasion.Gameplay.Items;
+using BlobbInvasion.UI;
+
+namespace BlobbInvasion.Tests
+{
+    public class testscript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void testscriptSimplePasses()
+        {
+            var type = Utils.ConvertCollectableType(CollectableType.BUL_BIG);
+
+            //wellwellwell
+
+            Assert.AreEqual(CraftingType.CORE,type);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator testscriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
